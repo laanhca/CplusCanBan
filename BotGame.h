8048e12452e9ec395b94271ef600e0308dbc7c8f
laanhca <anhlv98@gmail.com>
2021-06-25 13:05:34 +0700

    f1

--- BotGame.h
+++ BotGame.h
@@ -5,8 +5,6 @@ class BotGame
 
 public:
 	BotGame();
-	//void(*handlerResult)(std::string playerResult);
-	std::string (*handlerTool)(std::string) =nullptr;
 	std::string handlerResult(std::string (*handlerTool)(std::string), std::string playerResult);
 private:
 	~BotGame();
